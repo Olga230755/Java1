7265cf5d3d7bc451a50a16a85ca51b1ee833ec26
Olga Fedotova <bursh1-ola@yandex.ru>
2021-11-21 23:34:24 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int receipt = 500000000;
+        int total = balance + receipt;
+        System.out.println(total);
+    }
+}
